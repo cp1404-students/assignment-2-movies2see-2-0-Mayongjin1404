ae9c8b7342cd83d9246c0b2044123d8e946de48e
Mayongjin1404 <139750588+Mayongjin1404@users.noreply.github.com>
2024-04-20 14:51:18 +0800

    Update moviecollection.py

--- moviecollection.py
+++ moviecollection.py
@@ -3,7 +3,21 @@
 
 # TODO: Create your MovieCollection class in this file
 
-
 class MovieCollection:
-    """..."""
-    pass
+    def __init__(self):
+        self.movies = []
+
+    def add_movie(self, movie):
+        self.movies.append(movie)
+
+    def load_movies(self, filename):
+        # Implementation depends on the file format, see the `movies.json` for reference
+        pass
+
+    def save_movies(self, filename):
+        # Similar to load, save the movies to a JSON file
+        pass
+
+    def sort_movies(self, key):
+        self.movies.sort(key=lambda movie: getattr(movie, key))
+
