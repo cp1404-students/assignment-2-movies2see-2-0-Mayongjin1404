f916d45452f7a3321d866bf7e537881765504578
Mayongjin1404 <139750588+Mayongjin1404@users.noreply.github.com>
2024-04-20 14:50:09 +0800

    Update movie.py

--- movie.py
+++ movie.py
@@ -1,9 +1,19 @@
 """..."""
 
-
-# TODO: Create your Movie class in this file
-
-
 class Movie:
-    """..."""
+    def __init__(self, title, year, category, is_watched=False):
+        self.title = title
+        self.year = year
+        self.category = category
+        self.is_watched = is_watched
+
+    def __str__(self):
+        return f"{self.title} - {self.category} ({self.year}) {'*' if not self.is_watched else ''}"
+
+    def watch(self):
+        self.is_watched = True
+
+    def unwatch(self):
+        self.is_watched = False
+
     pass
